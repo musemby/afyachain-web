4c4c0ad4a44f8538c2b0c39cb9e6f4ceee94f697
musembi <musembinzioki@gmail.com>
2018-08-01 12:59:16 +0300

    Clean up: Removed pages/ui

--- src/app/pages/pages.module.js
+++ src/app/pages/pages.module.js
@@ -5,7 +5,7 @@
     'ui.router',
 
     'BlurAdmin.pages.dashboard',
-    'BlurAdmin.pages.ui',
+    // 'BlurAdmin.pages.ui',
     'BlurAdmin.pages.components',
     'BlurAdmin.pages.form',
     'BlurAdmin.pages.tables',
