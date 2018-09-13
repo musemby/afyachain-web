0bc49bbb2f53c16ee57966895ae7e44f032b559a
musembi <musembinzioki@gmail.com>
2018-09-13 18:44:15 +0300

    Return token control logic to login

--- src/app/pages/dashboard/dashboard.ctrl.js
+++ src/app/pages/dashboard/dashboard.ctrl.js
@@ -1,3 +1,4 @@
+
 (function () {
     'use strict';
         
@@ -5,16 +6,16 @@
         .controller('DashboardCtrl', ['$rootScope', '$scope', '$state', '$location', 'ParticipantsService', DashboardCtrl]);
         
     function DashboardCtrl($rootScope, $scope, $state, $location, patSvc) {
-        var token = $state.params.token;
-        if(token) {
-            Cookies.set('afyatoken', token);
-        }
-        var email = atob(token || Cookies.get('afyatoken'));
-        patSvc.get(email)
-        .then(function (data) {
-            $rootScope.user = data.data;
-        }).catch(function (err) {
-            console.log(err);
-        })
+        // var token = $state.params.token;
+        // if(token) {
+        //     Cookies.set('afyatoken', token);
+        // }
+        // var email = atob(token || Cookies.get('afyatoken'));
+        // patSvc.get(email)
+        // .then(function (data) {
+        //     $rootScope.user = data.data;
+        // }).catch(function (err) {
+        //     console.log(err);
+        // })
     }
 })();
